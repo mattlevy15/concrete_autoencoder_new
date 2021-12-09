1e33ab7035b3d63903943a2204c239b0ed2134e5
mattlevy15 <mattlevy15@gmail.com>
2021-12-09 13:22:20 -0600

    Update setup.py

--- concrete_autoencoder/setup.py
+++ concrete_autoencoder/setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="An implementation of Concrete Autoencoders",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mattlevy15/Concrete-Autoencoders/concrete_autoencoder_new",
+    url="https://github.com/mattlevy15/concrete_autoencoder_new",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
